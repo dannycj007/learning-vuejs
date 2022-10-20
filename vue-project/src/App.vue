6b63177bef1fd52c0c173dc1300f53e15a4c5d21
dannycj007 <dannyvoncharles007@gmail.com>
2022-10-20 09:23:34 +0300

    pagination

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,16 +1,111 @@
-<script setup>
-
+<script >
+import axios from "axios";
+export default {
+  data() {
+    return {
+      items: [],
+      paginate: ["items"],
+    };
+  },
+  created() {
+    this.loadPressRelease();
+  },
+  methods: {
+    loadPressRelease() {
+      axios.get(`https://zbeta2.mykuwaitnet.net/backend/en/api/v2/media-center/press-release/?page_size=61&type=5`)
+        .then((response) => {
+          this.items = response.data.results;
+        });
+    }
+  }
+};
 </script>
 
 <template>
-  <header>
-  </header>
-
-  <main>
-  
-  </main>
+  <div id="app">
+    <paginate ref="paginator" class="flex-container" name="items" :list="items">
+      <li v-for="(item, index) in paginated('items')" :key="index" class="flex-item">
+        <h4>{{ item.pub_date }}, {{ item.title }}</h4>
+        <img :src="item.image && item.image.file" />
+        <div class="downloads">
+          <span v-for="downloadable in item.downloadable.filter(
+            (d) => !!d.document_en
+          )" :key="downloadable.id">
+            <a :href="downloadable.document_en.file">Download</a>
+          </span>
+        </div>
+      </li>
+    </paginate>
+    <paginate-links for="items" :limit="2" :show-step-links="true"></paginate-links>
+  </div>
 </template>
 
 <style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
+ul.flex-container {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  flex-flow: row wrap;
+  flex-direction: row wrap;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+li img {
+  display: initial;
+  height: 100px;
+}
+
+.flex-item {
+  background: tomato;
+  width: calc(100% / 3.5);
+  padding: 5px;
+  height: auto;
+  margin-top: 10px;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+}
+
+.downloads {
+  margin-top: 10px;
+}
+
+ul.paginate-links.items li {
+  display: inline-block;
+  margin: 5px;
+}
+
+ul.paginate-links.items a {
+  cursor: pointer;
+}
+
+ul.paginate-links.items li.active a {
+  font-weight: bold;
+}
+
+ul.paginate-links.items li.next:before {
+  content: " | ";
+  margin-right: 13px;
+  color: #ddd;
+}
+
+ul.paginate-links.items li.disabled a {
+  color: #ccc;
+  cursor: no-drop;
+}
 </style>
---
+++ vue-project/src/components/icons/Header.vue
@@ -0,0 +1,87 @@
+<script type="text/javascript">
+    $(function(){
+        $('.menu-icon-show').click(function () {
+            $('.login-nav ul').fadeToggle();
+        });
+                /*$('.menu-overlay').click(function(){
+        $('.login-nav ul').fadeOut();
+
+    $(this).fadeOut();
+    return false;
+                })*/
+
+    $('.advanced-field').hide();
+    $('.advance-search').click(function () {
+        $('.advanced-field').slideToggle()
+    });
+            })
+
+</script>
+
+<style>
+#in-header-middle-wrap {
+       display: inline-block;
+       min-height: 50px;
+       width: 100%;
+       margin-top: 10px;
+       background: #222222;
+   }
+
+</style>
+
+<template>
+<header>
+ 
+    <div class="menu-overlay"></div>
+    
+    <nav class="main-nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-lg-2">
+                    <div class="menu-icon">
+                        <a class="menu-icon-show"><i class="fa fa-bars"></i></a>
+                        <a class="menu-icon-hide" style="display: none;"><i class="fa fa-times"></i></a>
+                    </div>
+                    <div class="site-logo">
+                        <a href="indexc6e3.html?ref=logo">
+                            <img class="big-logo" src="assets/img/logo.png" alt="logo" />
+                            <img class="small-logo" src="assets/img/logo.png" alt="logo" />
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-8 col-lg-8">
+                </div>
+                <div class="col-md-2 col-lg-2">
+                    <div class="right-nav">
+                        <!-- <ul>
+                            <li><a href="/" class="cart">Home</a></li>
+                            <li><a href="/shop" class="cart">Buy Container</a></li>
+                            <li><a href="/about" class="cart">About Us</a></li>
+                            <li><a href="/career" class="cart">Career</a></li>
+                            <li><a href="/contact" class="cart">Contact Us</a></li>
+                            <li><a href=""><i class="fa fa-search"></i></a></li>
+                            <li><a href="/shop/cart" class="cart"><i class="fa fa-shopping-cart text-white"></i><span class="text-white"> Cart (0)</span></a></li>
+                        </ul> -->
+                        <div class="login-nav" style="margin-right: 30px;">
+                            <ul class="right-nav-li">
+                                <!--<li>
+                                        <a class="login-button" href="/userauth/login">
+                                            <i class="fa fa-heart"></i> Favourite(0)
+                                        </a>
+                                    </li>-->
+                                <li>
+                                    <a class="login-button" href="userauth/login.html">
+                                        <i class="fa fa-lock"></i> Login / signup
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="clear"></div>
+        </div>
+    </nav>
+</header>
+</template>
